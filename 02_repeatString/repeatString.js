b7eb89a8c7d8edecb4167d3712b2db77a74f7e56
Rubiks14 <3637169+Rubiks14@users.noreply.github.com>
2023-08-03 22:49:37 -0500

    Complete first four exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (num < 0) { return 'ERROR'; }
+    if (num === 0) { return ''; }
 
+    let stringRepeated = '';
+    for (let i = 0; i < num; ++i) {
+        stringRepeated += string;
+    }
+    return stringRepeated;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let characterArray = string.split('');
+    return characterArray.reverse().join('');
 };
 
 // Do not edit below this line
