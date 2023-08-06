f953ba086db33e84eb7423356c4e3c76c5dd92de
Rubiks14 <3637169+Rubiks14@users.noreply.github.com>
2023-08-06 11:39:25 -0500

    Solve exercise 4
    I took a roundabout way of solving this. After looking at the solution I realized that both stated results are much simpler
    and easier to understand.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...thingsToRemove) {
 
+    let newArray = array;
+    for (let i = 0; i < thingsToRemove.length; ++i) {
+        const removeIndex = newArray.indexOf(thingsToRemove[i]);
+
+        if (0 <= removeIndex) {
+            newArray = [...newArray.slice(0, removeIndex), ...newArray.slice(removeIndex + 1)];
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
