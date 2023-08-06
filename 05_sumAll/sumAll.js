e53e8d16faba4724537fa952f59d3b48dc28b1f3
Rubiks14 <3637169+Rubiks14@users.noreply.github.com>
2023-08-06 11:50:20 -0500

    Solve exercise 5
    I used Math.min and Math.max to get my values. it seemed the most straight forward approach to me. I should remover the array
    destructing method though as that will come in handy.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (first, last) {
+    if (typeof first !== 'number' || typeof last !== 'number') {
+        return "ERROR";
+    }
+    else if (first < 0 || last < 0) {
+        return "ERROR";
+    } else {
+        min = Math.min(first, last);
+        max = Math.max(first, last);
 
+        let sum = 0;
+        for (let i = min; i <= max; ++i) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
